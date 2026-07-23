2c1a6043d374687b2369574e7f226836d427376b
yunhyeokd <yunhyeokd@gmail.com>
2026-07-23 13:10:16 +0900

    fix: 종목 검색 API 경로를 /api/stocks에서 /stocks로 수정

--- src/main/java/com/kbinvestory/backend/market/presentation/controllers/StockController.java
+++ src/main/java/com/kbinvestory/backend/market/presentation/controllers/StockController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/stocks")
+@RequestMapping("/stocks")
 public class StockController {
 
     private final MarketDataService marketDataService;
