0e9077174fc1983a3c885e186503ee4a8452d6d7
yunhyeokd <yunhyeokd@gmail.com>
2026-07-31 10:14:38 +0900

    fix: @EnableTransactionManagement 활성화
    PlatformTransactionManager 빈은 등록되어 있었지만 @EnableTransactionManagement가
    켜져 있지 않아 도메인 서비스의 @Transactional이 무시되고 있었다.

--- src/main/java/com/investory/global/database/DatabaseConfig.java
+++ src/main/java/com/investory/global/database/DatabaseConfig.java
@@ -14,10 +14,12 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement
 @PropertySource("classpath:application.properties")
 @MapperScan(basePackages = "com.investory", annotationClass = Mapper.class)
 public class DatabaseConfig {
